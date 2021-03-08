264e5f7dbeaa1b93263ec20789fb9c6353eefa53
Lewis0305 <lewiss4@oregonstate.edu>
2021-03-07 19:34:04 -0800

    LY Test Multiples of 100 but not 400

--- test_leap_year.py
+++ test_leap_year.py
@@ -14,3 +14,8 @@ class Testcase(unittest.TestCase):
         self.assertEqual(ly.leap_year(80), "Yes")
         self.assertEqual(ly.leap_year(48), "Yes")
         self.assertEqual(ly.leap_year(8), "Yes")
+
+    def test_Multi100_NonMulti400(self):
+        self.assertEqual(ly.leap_year(200), "Yes")
+        self.assertEqual(ly.leap_year(500), "Yes")
+        self.assertEqual(ly.leap_year(1100), "Yes")
