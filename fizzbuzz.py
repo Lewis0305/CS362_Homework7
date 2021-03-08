61622c97156ac116a79145d21645f3fb06dfb878
Lewis0305 <lewiss4@oregonstate.edu>
2021-03-07 22:32:07 -0800

    Added Print Statements

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,11 @@
 def fizzbuzz(number):
     if number%3 == 0 and number%5 ==0:
+        print("FizzBuzz")
         return "FizzBuzz"
     elif number%5 == 0:
+        print("Buzz")
         return "Buzz"
     elif number%3 == 0:
+        print("Fizz")
         return "Fizz"
     return number
--- leap_year.py
+++ leap_year.py
@@ -2,10 +2,14 @@ def leap_year(year):
     if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
+                print("Yes")
                 return "Yes"
             else:
+                print("No")
                 return "No"
         else:
+            print("Yes")
             return "Yes"
     else:
+        print("No")
         return "No"
