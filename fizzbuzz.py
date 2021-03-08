1c8a8bee302b974f8497f62334c4c7cfeec451fe
Lewis0305 <lewiss4@oregonstate.edu>
2021-03-07 19:41:43 -0800

    Cleaned Fizzbuzz File

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,8 @@
 def fizzbuzz(number):
-    if(number%3 == 0 and number%5 ==0):
+    if number%3 == 0 and number%5 ==0:
         return "FizzBuzz"
-    elif (number%5 == 0):
+    elif number%5 == 0:
         return "Buzz"
-    elif (number%3 == 0):
+    elif number%3 == 0:
         return "Fizz"
     return number
