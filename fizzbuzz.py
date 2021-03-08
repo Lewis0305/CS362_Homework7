bad945217d1f9211b8ea26f5c84410155794569e
Lewis0305 <lewiss4@oregonstate.edu>
2021-03-07 19:04:37 -0800

    Wrote first FB test for returning numbers

---
+++ fizzbuzz.py
@@ -0,0 +1,2 @@
+def fizzbuzz(Number):
+    pass
---
+++ test_fizzbuzz.py
@@ -0,0 +1,26 @@
+import unittest
+import fizzbuzz as fb
+
+class Testcase(unittest.TestCase):
+  if __name__ == '__main__':
+    unittest.main()
+
+    def test_None(self):
+        self.assertEqual(fb.fizzbuzz(1), 1)
+        self.assertEqual(fb.fizzbuzz(11), 11)
+        self.assertEqual(fb.fizzbuzz(13), 13)
+
+    #def test_fizzes(self):
+    #    self.assertEqual(fb.fizzbuzz(), "Fizz")
+    #    self.assertEqual(fb.fizzbuzz(), "Fizz")
+    #    self.assertEqual(fb.fizzbuzz(), "Fizz")
+
+    #def test_Buzzes(self):
+    #    self.assertEqual(fb.fizzbuzz(5), "Buzz")
+    #    self.assertEqual(fb.fizzbuzz(10), "Buzz")
+    #    self.assertEqual(fb.fizzbuzz(20), "Buzz")
+
+    def test_Fizzbuzzes(self):
+        self.assertEqual(fb.fizzbuzz(15), "FizzBuzz")
+        self.assertEqual(fb.fizzbuzz(30), "FizzBuzz")
+        self.assertEqual(fb.fizzbuzz(45), "FizzBuzz")
